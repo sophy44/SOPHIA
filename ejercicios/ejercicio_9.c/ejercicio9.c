35b1ede745e8fb4bd10ba445d2e9f61ef82416ef
sophy44 <projectsofia01@gmail.com>
2026-01-31 12:29:01 -0500

    nuevo ejercicio 9

--- ejercicios/ejercicio_9.c/ejercicio9.c
+++ ejercicios/ejercicio_9.c/ejercicio9.c
@@ -1,10 +1,39 @@
-#include  <stdio.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void solicitar_palabra(char *palabras)
+{
+    printf("ingrese una palabra: ");
+    scanf("%[^\n]%*c", palabras);
+}
+
+void contar(char *palabra)
+{
+    int letras = 0;
+    int espacios = 0;
+    int digitos = 0;
+    
+    for (int i = 0; i < 51; i++)
+    {
+        if(isalpha(palabra[i])){
+            letras++;
+        }
+        else if(isdigit(palabra[i])){
+            digitos++;
+        }
+        else if(palabra[i] == ' '){
+            espacios++;
+        }
+    }
+
+    printf("Letras: %d, digitos: %d, espacios en blanco: %d", letras, digitos, espacios);
+}
 
 int main()
 {
-    
- return  0;
+    char palabras[51] = "";
+    solicitar_palabra(palabras);
+    contar(palabras);
+
+    return 0;
 }
-
-
- 
